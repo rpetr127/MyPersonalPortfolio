712c6cdb595553ffb510e2e6a91bc17738ec74a4
peter7rom-star <nalrom4ever@gmail.com>
2023-03-02 11:15:52 +0300

    Add path for admin panel view file

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from ..views import AdminLoginView
+from views import AdminLoginView
 
 
 import blog
